40cf94ab43fe226e69aafec15e09bd31210bfc70
Md Saifuddullah <sdtutulctg@gmail.com>
2026-01-17 06:11:45 +0200

    type-alias

--- module1/src/typeAlias.ts
+++ module1/src/typeAlias.ts
@@ -1,16 +1,28 @@
 {
     // Type Alias
-    const student1:{
+    type Student = {
         name:string,
         age:number,
         gender:string,
-        phone:string,
+        phone?:string,
         address:string
-    } = {
+    }
+    const student1: Student = {
         name:"saif",
         age:24,
         gender:"male",
         phone:"+358418",
         address: "Helsinki"
     }
+    const student2: Student = {
+        name:"jabu",
+        age:27,
+        gender:"male",
+        address: "Doha"
+    }
+
+    // How to use type alias in function
+
+    type Add = (num1:number,num2:number)=>number;
+    const add:Add = (num1,num2) => num1 + num2;
 }
